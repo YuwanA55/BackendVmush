8835afd8037a5f50e82c5bc7c7a60c974b852b3f
YuwanM20 <anyougitr@gmail.com>
2025-04-15 00:22:36 +0700

    Pembaruan Database

--- DB/backendvmush.sql
+++ DB/backendvmush.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Apr 10, 2025 at 02:20 PM
+-- Generation Time: Apr 14, 2025 at 04:59 PM
 -- Server version: 8.0.30
 -- PHP Version: 8.1.10
 
@@ -40,16 +40,15 @@ CREATE TABLE `admin` (
 --
 
 CREATE TABLE `akun_user` (
-  `id_user` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `username` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `nama` varchar(150) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-  `email` varchar(100) NOT NULL,
+  `email` varchar(150) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `password` varchar(150) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   `status` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-  `image` varchar(150) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-  `Kota` varchar(150) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `gambar` varchar(150) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   `alamat` varchar(150) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-  `no_hp` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-  `tgl_regis` datetime DEFAULT NULL,
+  `nohp` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `tanggal_create` datetime DEFAULT NULL,
   `status_akun` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
@@ -57,8 +56,10 @@ CREATE TABLE `akun_user` (
 -- Dumping data for table `akun_user`
 --
 
-INSERT INTO `akun_user` (`id_user`, `nama`, `email`, `password`, `status`, `image`, `Kota`, `alamat`, `no_hp`, `tgl_regis`, `status_akun`) VALUES
-('USER0001', 'Yuwandana', 'yuwam147@gmail.com', 'yuwan147', 'Admin', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCFztnylePldHuz27yD5LrVHy8tA66JV7a7g&s', 'Kotakulon', 'KOTAKULON', '085607843865', '2025-04-09 21:51:04', 'Aktif');
+INSERT INTO `akun_user` (`username`, `nama`, `email`, `password`, `status`, `gambar`, `alamat`, `nohp`, `tanggal_create`, `status_akun`) VALUES
+('Danapay', 'Dana', 'Danayu@gmail.com', 'dana123', 'User', 'http://127.0.0.1:8000/GambarProfile/20250413_67fb66b8f23ea.jpeg', 'Kademangan', '085555555555', '2025-04-13 07:24:40', 'Aktif'),
+('Ragielpay', 'Ragiel', 'Ragiel123@gmail.com', 'Ragiel123', 'User', 'http://127.0.0.1:8000/GambarProfile/20250413_67fb6e2b8272f.jpeg', 'Kademangan', '085555555555', '2025-04-13 07:56:27', 'Aktif'),
+('Yuwanpay', 'Yuwandana', 'yuwam147@gmail.com', 'yuwan147', 'Admin', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCFztnylePldHuz27yD5LrVHy8tA66JV7a7g&s', 'KOTAKULON', '085607843865', '2025-04-09 21:51:04', 'Aktif');
 
 -- --------------------------------------------------------
 
@@ -68,7 +69,7 @@ INSERT INTO `akun_user` (`id_user`, `nama`, `email`, `password`, `status`, `imag
 
 CREATE TABLE `firebase` (
   `id` varchar(20) NOT NULL,
-  `id_user` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `username` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `Link` varchar(200) NOT NULL,
   `tanggal_create` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
@@ -77,8 +78,9 @@ CREATE TABLE `firebase` (
 -- Dumping data for table `firebase`
 --
 
-INSERT INTO `firebase` (`id`, `id_user`, `Link`, `tanggal_create`) VALUES
-('FRB0001', 'USER0001', 'https://www.youtube.com/watch?v=2STQ7jPN0v0&t=440s', '2025-04-10 13:01:00');
+INSERT INTO `firebase` (`id`, `username`, `Link`, `tanggal_create`) VALUES
+('FRB0001', 'yuwanpay', 'https://www.youtube.com/watch?v=2STQ7jPN0v0&t=440s', '2025-04-10 13:01:00'),
+('FRB0002', 'Ragielpay', 'https://www.youtube.com/results?search_query=dj+viral+tiktok+2025', '2025-04-14 22:56:00');
 
 -- --------------------------------------------------------
 
@@ -88,14 +90,22 @@ INSERT INTO `firebase` (`id`, `id_user`, `Link`, `tanggal_create`) VALUES
 
 CREATE TABLE `paket` (
   `id_paket` varchar(10) NOT NULL,
-  `nama_paket` varchar(150) NOT NULL,
-  `harga_paket` varchar(150) NOT NULL,
-  `jumlah_sensor` varchar(150) NOT NULL,
-  `kontrol_app` varchar(150) NOT NULL,
-  `support` varchar(150) NOT NULL,
-  `analisisdata` varchar(150) NOT NULL,
-  `konsultasi` varchar(150) NOT NULL
+  `nama_paket` varchar(150) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `harga` varchar(150) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `jumlah_sensor` varchar(150) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `kontrol_app` varchar(150) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `support` varchar(150) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `analisisdata` varchar(150) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `konsultasi` varchar(150) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `gambar` varchar(250) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `paket`
+--
+
+INSERT INTO `paket` (`id_paket`, `nama_paket`, `harga`, `jumlah_sensor`, `kontrol_app`, `support`, `analisisdata`, `konsultasi`, `gambar`) VALUES
+('PKT0001', 'Paket Rakyat', '199', '1', 'Kontrol basic via App', 'Support 8/5', NULL, NULL, '');
 
 -- --------------------------------------------------------
 
@@ -105,11 +115,18 @@ CREATE TABLE `paket` (
 
 CREATE TABLE `pembelian` (
   `id` varchar(20) NOT NULL,
-  `id_paket` varchar(20) NOT NULL,
-  `id_user` varchar(20) NOT NULL,
+  `id_paket` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `username` varchar(250) DEFAULT NULL,
   `status` varchar(50) DEFAULT NULL,
-  `tanggal` datetime NOT NULL
+  `tanggal` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `pembelian`
+--
+
+INSERT INTO `pembelian` (`id`, `id_paket`, `username`, `status`, `tanggal`) VALUES
+('PBL0001', 'PKT0001', 'Danapay', NULL, '2025-04-14 23:20:30');
 
 -- --------------------------------------------------------
 
@@ -148,14 +165,14 @@ CREATE TABLE `tengkulak` (
 -- Indexes for table `akun_user`
 --
 ALTER TABLE `akun_user`
-  ADD PRIMARY KEY (`id_user`);
+  ADD PRIMARY KEY (`username`);
 
 --
 -- Indexes for table `firebase`
 --
 ALTER TABLE `firebase`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `hub_link_user` (`id_user`);
+  ADD KEY `hub_link_user` (`username`);
 
 --
 -- Indexes for table `paket`
@@ -168,8 +185,8 @@ ALTER TABLE `paket`
 --
 ALTER TABLE `pembelian`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `id_paket` (`id_paket`),
-  ADD UNIQUE KEY `id_user` (`id_user`);
+  ADD KEY `hub_Pembelian_user` (`username`),
+  ADD KEY `hub_paket` (`id_paket`);
 
 --
 -- Constraints for dumped tables
@@ -179,14 +196,14 @@ ALTER TABLE `pembelian`
 -- Constraints for table `firebase`
 --
 ALTER TABLE `firebase`
-  ADD CONSTRAINT `hub_link_user` FOREIGN KEY (`id_user`) REFERENCES `akun_user` (`id_user`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+  ADD CONSTRAINT `hub_link_user` FOREIGN KEY (`username`) REFERENCES `akun_user` (`username`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 
 --
 -- Constraints for table `pembelian`
 --
 ALTER TABLE `pembelian`
   ADD CONSTRAINT `hub_paket` FOREIGN KEY (`id_paket`) REFERENCES `paket` (`id_paket`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  ADD CONSTRAINT `hub_Pembelian_user` FOREIGN KEY (`id_user`) REFERENCES `akun_user` (`id_user`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+  ADD CONSTRAINT `hub_Pembelian_user` FOREIGN KEY (`username`) REFERENCES `akun_user` (`username`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;

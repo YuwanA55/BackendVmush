5524f2586956958ce9c4d713c6e616b46b168640
yuwanpay <95163223+YuwanA55@users.noreply.github.com>
2025-05-26 03:28:23 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,5 @@
-#!/bin/sh
+# Perbaiki permission sebelum menjalankan php-fpm
+chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
+chmod -R 775 /var/www/html/storage /var/www/html/bootstrap/cache
 
-php artisan migrate --force
-php artisan config:cache
-php artisan route:cache
-php artisan view:cache
-
-php-fpm
+exec "$@"
